0c7e3b25896e4889b96dcb18c73540ad7e6f472a
Kopnu <dzivv0@yandex.ru>
2026-06-01 00:52:29 +0300

    Стили

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { i18n } from './i18n'
-import './style.css'
+import './styles/base.css'
+import './styles/app.css'
+import './styles/shared.css'
+import './styles/alloy.css'
+import './styles/ironworking.css'
+import './styles/prospector.css'
 
 createApp(App).use(i18n).mount('#app')
